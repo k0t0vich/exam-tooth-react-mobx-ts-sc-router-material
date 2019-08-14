dab03f82bf6c9b0b04396b7d024edd4538b55f29
Konstantin Bondarenko <k.bondarenko@corp.mail.ru>
2019-08-14 12:22:36 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import './App.css';
+
+export default class App extends Component {
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+          exam-tooth-react-mobx-ts-sc-router-material
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
